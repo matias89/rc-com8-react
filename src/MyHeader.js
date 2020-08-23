0a127e8599f92926dd93d1467a1ce7bcc33bcf53
Santiago <you@example.com>
2020-08-23 18:46:30 -0300

    avance

---
+++ src/MyHeader.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+//import MyButton from './MyButton';
+
+class MyHeader extends Component {
+    render() {
+        const { content, color, onClick } = this.props;
+
+        return (
+            <header className="el-nombre-de-la-clase">
+                <h1 style={{color}}>{content}</h1>
+                
+            </header>
+        );
+    }
+}
+
+MyHeader.defaultProps = {
+    color: 'yellow'
+}
+
+export default MyHeader;
+//<MyButton onClick={onClick} show className="clase-custom" title="Botón en el header" color="green" />
